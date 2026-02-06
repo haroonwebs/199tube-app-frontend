55f5cbbaa7c6f03eaebb38ca0a1394c627363030
haroontalib <haroontalib8029@gmail.com>
2026-02-06 23:57:06 +0500

    setup useMutations hook to call logout api

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,13 +2,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { Logout } from "../apiData/logout";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
   const [user, setUser] = useState<string | null>(null);
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    setUser(storedUser);
-  }, []);
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: Logout,
+    onSuccess: () => {
+      queryClient.clear();
+      router.push("/login");
+    },
+  });
+
+  const handleLogout = () => {
+    mutate();
+  };
 
   return (
     <div className="flex justify-between items-center px-5 h-15 bg-[#0E0E0F] text-xl text-white">
@@ -22,15 +34,18 @@ const Header = () => {
         />
       </Link>
       {user ? (
-        <Link href={"/user-dashboard"}>
-          <Image
-            className=" rounded-lg object-cover border-0 hover:border-2 hover:border-amber-50"
-            src="/haroon.jpg"
-            alt="Banner"
-            width={50}
-            height={50}
-          />
-        </Link>
+        <div>
+          <Link href={"/user-dashboard"}>
+            <Image
+              className=" rounded-lg object-cover border-0 hover:border-2 hover:border-amber-50"
+              src="/haroon.jpg"
+              alt="Banner"
+              width={50}
+              height={50}
+            />
+          </Link>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
       ) : (
         <Link href={"/auth/login"}>Login</Link>
       )}
